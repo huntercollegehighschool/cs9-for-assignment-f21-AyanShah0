d35fe2ad8bc078a83821d4ab5ae14b8caf901ea7
Ayan Shah <93940099+AyanShah0@users.noreply.github.com>
2022-01-07 17:32:29 +0000

    1: Changed product to 1 so that it wouldn't keep multiplying into 0. Made product multiply itself by i+1 instead of i because the first i would've been 0, making everything else 0.

--- part2.py
+++ part2.py
@@ -8,3 +8,10 @@ For example, if the user types in 4, the program should print 100 (since 1^3 + 2
 """
 
 #write your code here
+n = int(input('Choose a positive whole number: '))
+answer = 0
+
+for i in range(n+1):
+  answer += i**3
+
+print(answer)
--- part4.py
+++ part4.py
@@ -40,3 +40,13 @@ fizz
 """
 
 # write your code here 
+number = int(input('Enter a number: '))
+for x in range(number+1):
+  if x % 3 == 0 and x % 5 == 0:
+    print('fizzbuzz')
+  elif x % 3 == 0:
+    print('fizz')
+  elif x % 5 == 0:
+    print('buzz')
+  else:
+    print(x)
